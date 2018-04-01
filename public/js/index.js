c16e41a3e7323fc8c727c2d9b3189e6da41f2de8
Trevor Hoy <trevorphoy@gmail.com>
2018-04-01 15:05:11 -0700

    format timestamps using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -10,19 +10,21 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
+  let formattedTime = moment(message.createdAt).format('h:mm a');
+
   console.log('NEW MESSAGE from server:', message);
   var li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
-  console.log(message);
+  let formattedTime = moment(message.createdAt).format('h:mm a');
   let li = $('<li></li>');
   let a = $('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
 
   li.append(a);
