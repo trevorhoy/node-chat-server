3f56a8cc8902d9e6700fbe9dadf7eb9b2e6bcc60
Trevor Hoy <trevorphoy@gmail.com>
2018-04-01 12:33:43 -0700

    Emit newMessage on createMEssage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    text: 'Creating a new email',
-    to: 'jen@example.com'
-  });
 });
 
 socket.on('disconnect', function () {
